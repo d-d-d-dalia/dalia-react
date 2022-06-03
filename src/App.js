08f3c21059b0f244c82d76b4dbfa9504dd94f387
Dalia Sawaya <dalia.sawaya@stitchfix.com>
2022-06-03 16:11:48 -0700

    part IV

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 
-import React from "react";
+import React, { useState } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
 
 function App(props) {
 
-  const taskList = props.tasks.map(task => (
-    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id}/>
+  const [tasks, setTasks] = useState(props.tasks)
+
+  const taskList = tasks.map(task => (
+    <Todo 
+    id={task.id} 
+    name={task.name} 
+    completed={task.completed} 
+    key={task.id} 
+    toggleTaskCompleted={toggleTaskCompleted}
+    deleteTask={deleteTask}
+    />
   ));
+
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false}
+    setTasks([...tasks, newTask])
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      if (id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task
+    })
+    setTasks(updatedTasks)
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task =>  id !== task.id)
+    setTasks(remainingTasks) 
+  }
+
+  const taskNoun = taskList.length !== 1 ? "tasks" : "task"
+  const headingText = `${taskList.length} ${taskNoun} remaining`
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         role="list"
