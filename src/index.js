8f85d051938e34d77b993ccdb9f15a786588ebbe
Dalia Sawaya <dalia.sawaya@stitchfix.com>
2022-06-03 15:17:52 -0700

    part III

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
+
 root.render(
-    <App subject="Clarice" />, document.getElementById('root')
+    <App tasks={DATA} />, document.getElementById('root')
 );
