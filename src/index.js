51fb882090eb11bfd665051b247d8d0405cf1493
Dalia Sawaya <dalia.sawaya@stitchfix.com>
2022-06-03 13:27:10 -0700

    complete exercise

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />, document.getElementById('root')
+    <App subject="Clarice" />, document.getElementById('root')
 );
