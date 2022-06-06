33898690f2436818eb48c1a5f500ac00a658fa5e
Dalia Sawaya <dalia.sawaya@stitchfix.com>
2022-06-06 16:15:55 -0700

    part V

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks)
+  const [filter, setFilter] = useState('All')
 
-  const taskList = tasks.map(task => (
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
     <Todo 
     id={task.id} 
     name={task.name} 
@@ -17,6 +28,16 @@ function App(props) {
     key={task.id} 
     toggleTaskCompleted={toggleTaskCompleted}
     deleteTask={deleteTask}
+    editTask={editTask}
+    />
+  ));
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+    key={name} 
+    name={name}
+    isPressed={name === filter}
+    setFilter={setFilter}
     />
   ));
 
@@ -40,6 +61,16 @@ function App(props) {
     setTasks(remainingTasks) 
   }
 
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      if (id === task.id) {
+        return {...task, name: newName}
+      }
+      return task
+    })
+    setTasks(editedTaskList)
+  }
+
   const taskNoun = taskList.length !== 1 ? "tasks" : "task"
   const headingText = `${taskList.length} ${taskNoun} remaining`
 
@@ -48,9 +79,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">
         {headingText}
